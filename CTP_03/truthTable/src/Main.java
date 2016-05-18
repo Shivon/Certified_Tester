064173cdd5de11f91e5feeb6961643ce44a4b23f
Shivon <marjan.bachtiari@haw-hamburg.de>
2016-05-18 23:34:19 +0200

    Rewrite due to stupid implementation

--- CTP_03/truthTable/src/Main.java
+++ CTP_03/truthTable/src/Main.java
@@ -1,22 +1,31 @@
+import java.io.Console;
+
 public class Main {
 
     public static void main(String[] args) {
-        int[] vec = new int[5];
-        System.out.println("Länge vec = " + vec.length);
-        System.out.println("Inhalt vec = " + vec[0]);
-
-        InputVector inputVector = new InputVector(5);
-        int[] vector = inputVector.getVector();
-        System.out.println("Länge vector = " + vector.length);
-        System.out.println("Inhalt vector = " + vector[2]);
-
-        inputVector.setVector("1,0,1,0,2");
-        System.out.println("vector: " + inputVector.vectorAsString());
-        int[] v = {1,0,10,5};
-        inputVector.onlyAllowValues(v);
-        System.out.println("allow: " + inputVector.allowedValuesAsString());
-        inputVector.setVector("1,0,1,0,10");
-        System.out.println("vector after allow: " + inputVector.vectorAsString());
-
+//        int[] vec = new int[5];
+//        System.out.println("Länge vec = " + vec.length);
+//        System.out.println("Inhalt vec = " + vec[0]);
+//
+//        InputVector inputVector = new InputVector(5);
+//        int[] vector = inputVector.getVector();
+//        System.out.println("Länge vector = " + vector.length);
+//        System.out.println("Inhalt vector = " + vector[2]);
+//
+//        inputVector.setVector("1,0,1,0,2");
+//        System.out.println("vector: " + inputVector.vectorAsString());
+//        int[] v = {1,0,10,5};
+//        inputVector.onlyAllowValues(v);
+//        System.out.println("allow: " + inputVector.allowedValuesAsString());
+//        inputVector.setVector("1,0,1,0,10");
+//        System.out.println("vector after allow: " + inputVector.vectorAsString());
+        Console console = System.console();
+        System.out.println("Welcome to truth table viewer!");
+        String numberOfRows = console.readLine("Enter the number of rows your table will have (excl result row): ");
+        int rows = Integer.parseInt(numberOfRows);
+        String numberOfColumns = console.readLine("Enter the number of columns your table will have: ");
+        int columns = Integer.parseInt(numberOfColumns);
+        TruthTableBeta beta = new TruthTableBeta(rows, columns, console);
+        beta.fillTable();
     }
 }
--- CTP_03/truthTable/src/TruthTable.java
+++ CTP_03/truthTable/src/TruthTable.java
@@ -1,9 +1,21 @@
 public class TruthTable {
     private int rowCount;
     private int columnCount;
+    private int[][] inputMatrice;
+    private int[] resultArray;
 
     public TruthTable(int rowCount, int columnCount) {
         this.rowCount = rowCount;
         this.columnCount = columnCount;
+//        for (int i = 0; i < rowCount; i++) {
+//
+//            this.inputMatrice[i] = new InputVector(columnCount);
+//        }
+        this.inputMatrice = new int[rowCount][columnCount];
+    }
+
+    public void setRow(int rowIndex, String inputString) {
+        InputVector inputVector = new InputVector(columnCount);
+        inputMatrice[rowIndex] = inputVector.setVector(inputString);
     }
 }
