1aef31bc18135f1412a30803523ba8d0fcd0de5d
Shivon <marjan.bachtiari@haw-hamburg.de>
2016-05-19 03:14:58 +0200

    Cleanup

--- CTP_03/truthTable/src/Main.java
+++ CTP_03/truthTable/src/Main.java
@@ -1,35 +1,18 @@
 import java.io.Console;
 
 public class Main {
-
     public static void main(String[] args) {
-//        int[] vec = new int[5];
-//        System.out.println("Länge vec = " + vec.length);
-//        System.out.println("Inhalt vec = " + vec[0]);
-//
-//        InputVector inputVector = new InputVector(5);
-//        int[] vector = inputVector.getVector();
-//        System.out.println("Länge vector = " + vector.length);
-//        System.out.println("Inhalt vector = " + vector[2]);
-//
-//        inputVector.setVector("1,0,1,0,2");
-//        System.out.println("vector: " + inputVector.vectorAsString());
-//        int[] v = {1,0,10,5};
-//        inputVector.onlyAllowValues(v);
-//        System.out.println("allow: " + inputVector.allowedValuesAsString());
-//        inputVector.setVector("1,0,1,0,10");
-//        System.out.println("vector after allow: " + inputVector.vectorAsString());
         Console console = System.console();
         System.out.println("Welcome to truth table viewer!");
-        String numberOfRows = console.readLine("Enter the number of rows your table will have (excl result row): ");
-        int rows = Integer.parseInt(numberOfRows);
-        String numberOfColumns = console.readLine("Enter the number of columns your table will have: ");
+        String numberOfColumns = console.readLine("Enter the number of atomic conditions your table will have (columns): ");
         int columns = Integer.parseInt(numberOfColumns);
-        TruthTableBeta beta = new TruthTableBeta(rows, columns, console);
-        beta.fillTable();
+        String numberOfRows = console.readLine("Enter the number of vectors your table will have (rows): ");
+        int rows = Integer.parseInt(numberOfRows);
+        TruthTable truthTable = new TruthTable(rows, columns, console);
+        truthTable.fillTable();
         System.out.println("\n");
-        beta.printTable();
-        System.out.println("\n");
-        beta.printTableBinary();
+//        truthTable.printTable();
+//        System.out.println("\n");
+        truthTable.printTableBinary();
     }
 }
