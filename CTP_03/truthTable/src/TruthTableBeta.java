cde5a3af75be73cdee0850adb6155f35f97208ea
Shivon <marjan.bachtiari@haw-hamburg.de>
2016-05-19 03:09:04 +0200

    Fix stuff

--- CTP_03/truthTable/src/TruthTableBeta.java
+++ CTP_03/truthTable/src/TruthTableBeta.java
@@ -3,10 +3,13 @@ import java.util.Arrays;
 
 
 public class TruthTableBeta {
+    // columns = vector of boolean values for each variable
+    // rows = all values for one variable
     private int rowCount;
     private int columnCount;
     private boolean[][] inputMatrice;
     private boolean[] resultArray;
+    private String[] conditionNames;
     private Console console;
 
     public TruthTableBeta(int rowCount, int columnCount, Console console) {
@@ -15,41 +18,50 @@ public class TruthTableBeta {
         this.console = console;
         this.inputMatrice = new boolean[rowCount][columnCount];
         this.resultArray = new boolean[columnCount];
+        this.conditionNames = new String[columnCount];
     }
 
     public void fillTable() {
-        for (int row = 0; row < rowCount; row++) {
-            for (int column = 0; column < columnCount; column++) {
-                String value = console.readLine("Enter boolean value for row " + row + " and column " + column + ":  ");
-                inputMatrice[row][column] = parseBoolFromString(value);
-            }
-            System.out.println("Row " + row + ": " + Arrays.toString(inputMatrice[row]));
+        // Define condition names
+        for (int column = 0; column < columnCount; column++) {
+            String var = console.readLine("Enter condition NAMES for column " + column + ":  ");
+            conditionNames[column] = var;
         }
 
         System.out.println("------------------------------------------------------");
 
-        for (int column = 0; column < columnCount; column++) {
-            String value = console.readLine("Enter boolean value for result row, column " + column + ":  ");
-            resultArray[column] = parseBoolFromString(value);
+        // Fill truth table itself
+        for (int row = 0; row < rowCount; row++) {
+            for (int column = 0; column < columnCount; column++) {
+                String value = console.readLine("Enter boolean value for condition (row) " + row + " and vector (column) " + column + ":  ");
+                inputMatrice[row][column] = parseBoolFromString(value);
+            }
+            System.out.println("Condition (row) " + row + ": " + Arrays.toString(inputMatrice[row]));
+        }
+
+        System.out.println("------------------------------------------------------");
+
+        // Fill result column
+        for (int row = 0; row < rowCount; row++) {
+            String value = console.readLine("Enter boolean values for result column, vector (row) " + row + ":  ");
+            resultArray[row] = parseBoolFromString(value);
         }
 
         System.out.println("Result row: " + Arrays.toString(resultArray));
     }
 
     public void printTable() {
+        System.out.println(formatForPrint(Arrays.toString(conditionNames)));
         for (int row = 0; row < rowCount; row++) {
-            System.out.println(Arrays.toString(inputMatrice[row]));
+            System.out.println(formatForPrint(Arrays.toString(inputMatrice[row])) + " | " + resultArray[row]);
         }
-        System.out.println("------------------------------------------------------");
-        System.out.println(Arrays.toString(resultArray));
     }
 
     public void printTableBinary() {
+        System.out.println(formatForPrint(Arrays.toString(conditionNames)));
         for (int row = 0; row < rowCount; row++) {
-            System.out.println(toBinaryString(inputMatrice[row]));
+            System.out.println(toBinaryString(inputMatrice[row]) + " | " + toBinaryString(resultArray[row]));
         }
-        System.out.println("------------------------------------------------------");
-        System.out.println(toBinaryString(resultArray));
     }
 
     // Getter
@@ -58,12 +70,16 @@ public class TruthTableBeta {
         return inputMatrice;
     }
 
-    public boolean[] getResultVector() {
+    public boolean[] getResultColumn() {
         return resultArray;
     }
 
-    public boolean[] getRow(int row) {
-        return inputMatrice[row];
+    public boolean getResultForVector(int rowIndex) {
+        return resultArray[rowIndex];
+    }
+
+    public boolean[] getVector(int rowIndex) {
+        return inputMatrice[rowIndex];
     }
 
     public boolean getCell(int row, int column) {
@@ -76,12 +92,12 @@ public class TruthTableBeta {
         inputMatrice = matrice;
     }
 
-    public void setResultVector(boolean[] result) {
+    public void setResultColumn(boolean[] result) {
         resultArray = result;
     }
 
-    public void setRow(int rowNumber, boolean[] rowContent) {
-        inputMatrice[rowNumber] = rowContent;
+    public void setVector(int rowIndex, boolean[] vector) {
+        inputMatrice[rowIndex] = vector;
     }
 
     public void setCell(int row, int column, boolean value) {
@@ -92,7 +108,19 @@ public class TruthTableBeta {
 
     private String toBinaryString(boolean[] row) {
         String rowString = Arrays.toString(row);
-        return rowString.replace("false", "0").replace("true", "1").replace(",", "");
+        return formatForPrint(rowString.replace("false", "0").replace("true", "1"));
+    }
+
+    private String toBinaryString(boolean bool) {
+        if (bool) {
+            return "1";
+        } else {
+            return "0";
+        }
+    }
+
+    private String formatForPrint(String string) {
+        return string.replace(",", "").replace("[", "").replace("]", "");
     }
 
     private boolean parseBoolFromString(String string) {
